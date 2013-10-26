4f142edbd439f9e4c921d6269e7d10dbacce7fd1
alex <alexander.vanyan@gmail.com>
2013-10-26 23:21:44 +0400

    initial import

---
+++ README.md
@@ -0,0 +1,63 @@
+progressr
+=========
+
+Yet another progressive loading jQuery plugin.  
+This plugin does what no other plugins do normally (without bugs) - it can work with separate item append and scroll containers (meaning those could be completely different 2 DOM elements). 
+Plus, there is some logic in this one to guess what selectors should be used instead, if some selectors do not exist (given wrong) or are omitted.
+
+Usage
+=====
+```html
+<script type="text/javascript">
+    $(document).ready(function() {
+        $("#my-scrollable-container").progressr({
+			loadContainer: "#my-repeatable-block-parent",
+			nextSelector: ".next-page-invisible-element",
+			singleItemSelector: ".my-repeatable-block",
+			fadeIn: 1500
+		});
+    });
+</script>
+<div id="my-scrollable-container">
+	a hell of content here...
+	...
+	...
+	...
+</div>
+<div id="my-repeatable-block-parent">
+	<div class="my-repeatable-block">rpt 1</div>
+	<div class="my-repeatable-block">rpt 2</div>
+	<div class="my-repeatable-block">rpt 3</div>
+	<div class="my-repeatable-block">rpt 4</div>
+</div>
+<a class="next-page-invisible-element" href="http://my-example-site.com/?page=2" style="display: none;"></a>
+```
+
+Demo and advanced usage
+-----------------------
+Demo available at: http://narek.ws/ (usage with WordPress)
+
+
+Options
+=======
+
+**loadContainer** [default: (jQuery object on which plugin was called, e.g. $("#my-scrollable-container") )] - jquery selector for the element which newly loaded content (block item elements) will be appended to  
+**singleItemSelector** [default: ".item"] - single item element selector string (block item element); the plugin will get items from the next page (e.g. /?page=2) according to this option  
+**nextSelector** [default: "a.next-page"] - selector string to element which has href attribute pointing to the next page (e.g. <a href="/?page=2"></a>); this element will be grabbed again from the newly loaded DOM each time a request to next page is made 
+**extraPixels** [default: 0] - extra added pixels from which progressive loading will occur (by default, loading will begin only after last bottom pixel has been reached)  
+**fadeIn** [default: false] - integer value meaning milliseconds after which newly loaded blocks will fade in after being appended to the "loadContainer" (by default, they will appear without delay and fade effect)  
+
+Callbacks
+=========
+
+**beforeLoad()** - is fired in case of success but before the newly loaded data has been appended to the "loadContainer"; receives no arguments  
+**afterLoad(newItems)** - is fired in case of success after the newly loaded data has been appended to the "loadContainer" and is displayed (after fadeIn effect); newItems jQuery elements array is passed as an argument (to know which are the new elements in the item stack)  
+**onFailure(data)** - works in case of AJAX request failure, receives "data" argument - response of XHR request  
+**onRequestComplete(data)** - works in case of any XHR request, has "data" argument - the response text  
+**onNoMoreItems()** - is triggered every time whenever bottom of the scrollable container has been reached (or bottom + extraPixels - depending on configuration option) but there are no more items to load  
+
+Notes
+=====
+
+Whenever the plugin does not find anything necessary for working it tries to "guess" it from the given DOM structure and process everything intuitively - be that a wrongly given selector string or a missed option in plugin configuration.  
+
---
+++ jquery.progressr.js
@@ -0,0 +1,100 @@
+/*
+ * Progressr - jQuery Plugin
+ * A progressive AJAX content loading library.
+ *
+ * Copyright (c) 2013 Alex Vanyan (http://alex-v.net)
+ * Version: 1.0
+ * Requires: jQuery v1.4.2+
+ *
+ */
+
+(function($) {
+
+    $.fn.progressr = function(options) {
+
+        return this.each(function() {
+
+            var $this = $(this);
+
+            // default options: do not change!
+            var defaults = {
+                loadContainer: $this,
+                singleItemSelector: ".item",
+                nextSelector: "a.next-page",
+                extraPixels: 0,
+                fadeIn: false,
+                beforeLoad: function() {},
+                afterLoad: function() {},
+                onFailure: function() {},
+                onRequestComplete: function() {},
+                onNoMoreItems: function() {}
+            };
+
+            var opts = $.extend( {}, defaults, options );
+            var extraPixels = parseInt(opts.extraPixels);
+            var loadContainer = (typeof opts.loadContainer === "string" ? $(opts.loadContainer) : opts.loadContainer);
+
+            // guesser method definitions:
+            // those methods are supposed to guess
+            // various jquery selectors/objects
+            var guessNextHref = function(obj) {
+                obj = obj.eq(0);
+                return ( obj.prop("tagName") == "A" ? obj : obj.find("a").eq(0) ).attr("href");
+            };
+            var guessSingleItem = function() {
+                return ( $(opts.singleItemSelector).length ? opts.singleItemSelector :
+                                                             '>' + (loadContainer.children().eq(0).attr("id") ?
+                                                             '#' + loadContainer.children().eq(0).attr("id") :
+                                                             loadContainer.children().eq(0).prop("tagName").toLowerCase()) );
+            };
+
+            var reqUrl = guessNextHref( $(opts.nextSelector) );
+            var singleItemSel = guessSingleItem();
+            var newItems;
+
+            // bind scroll event to the selector
+            // object on which plugin function
+            // was called on
+            $this.scroll(function() {
+                // if no URL string is given, we've reached the last page
+                if ( typeof reqUrl === "undefined" && ! reqUrl ) {
+                    opts.onNoMoreItems.call();
+                    return false;
+                }
+                // check if we have reached to the end of the scroll container
+                if ( $(this).scrollTop() + $(this).height() + extraPixels >= $(this)[0].scrollHeight ) {
+                    // process the GET type ajax request
+                    $.ajax({
+                        type: "GET",
+                        url: reqUrl
+                    }).done(function(data) { // in case of success
+                        var respObj = $(data);
+                        // pre item load callbacks
+                        opts.beforeLoad.call();
+                        // reassign the url variable
+                        // to be given to next ajax request object
+                        reqUrl = guessNextHref( respObj.find(opts.nextSelectors) );
+                        // find the newly loaded items and append
+                        // to "loadContainer" element (defined in options)
+                        newItems = respObj.find(singleItemSel).hide();
+                        loadContainer.append(newItems);
+                        // show items immediately or fade in based on "fadeIn" option value
+                        newItems.fadeIn(opts.fadeIn ? parseInt(opts.fadeIn) : 0, function() {
+                            // post item load callbacks
+                            opts.afterLoad.call(this, newItems);
+						});
+                    }).fail(function(data) { // in case of failure
+                        // proceed to callback
+                        opts.onFailure.call(this, data);
+                    }).always(function(data) { // always called after request
+                        // proceed to callback
+                        opts.onRequestComplete.call(this, data);
+                    });
+                }
+            });
+
+        });
+
+    };
+
+})(jQuery);
